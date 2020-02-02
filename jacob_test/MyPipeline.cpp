18ab318f00369b87e2600a008528e4b47678a63b
Mateo Silver <mateonsilver@gmail.com>
2020-02-02 16:45:17 -0600

    Implementing math and stuff -jacob

--- jacob_test/MyPipeline.cpp
+++ jacob_test/MyPipeline.cpp
@@ -7,6 +7,11 @@ const double MyPipeline::thresh_val[] = {30, 255};
 const int MyPipeline::erosionSize = 0;
 const int MyPipeline::dilationSize = 2;
 
+const double MyPipeline::targetHeight = 78.5;  // dy in image - from target height to camera height (e.g. 500cm)
+const double MyPipeline::angleOffset = 60.0;   // Angle that the camera is set to relative the ground (e.g. perpendicular to the ground is 90deg)
+const double MyPipeline::verticalFOV = 48.8;   // Vertical field of view of the camera
+const double MyPipeline::horizontalFOV = 62.2; // Horizontal field of view of the camera
+
 MyPipeline::MyPipeline()
     : contourResults()
 {
@@ -20,13 +25,12 @@ void MyPipeline::Process(cv::Mat &mat)
     erodeSize(mat, mat, erosionSize);
     dilateSize(mat, mat, dilationSize);
 
-    pipelineContours(mat, contourResults);
+    drawAndUpdate(mat, contourResults);
 
+    trajectory(mat);
     wpi::outs() << "Contours count: " << contourResults.size() << "\n";
 
     // Did the math...main.cpp will send these to RIO
-    resultA = 0.25;
-    resultB = 0.75;
 }
 
 void MyPipeline::thresholdHSV(cv::Mat &input, cv::Mat &output,
@@ -39,26 +43,98 @@ void MyPipeline::thresholdHSV(cv::Mat &input, cv::Mat &output,
 void MyPipeline::erodeSize(cv::Mat &input, cv::Mat &output, int size)
 {
     erode(input, output,
-            getStructuringElement(
-                cv::MORPH_ELLIPSE,
-                cv::Size(2 * erosionSize + 1, 2 * erosionSize + 1),
-                cv::Point(erosionSize, erosionSize)
-            ));
+          getStructuringElement(
+              cv::MORPH_ELLIPSE,
+              cv::Size(2 * erosionSize + 1, 2 * erosionSize + 1),
+              cv::Point(erosionSize, erosionSize)));
 }
 
 void MyPipeline::dilateSize(cv::Mat &input, cv::Mat &output, int size)
 {
     dilate(input, output,
-            getStructuringElement(
-                cv::MORPH_ELLIPSE,
-                cv::Size(2 * dilationSize + 1, 2 * dilationSize + 1),
-                cv::Point(dilationSize, dilationSize)
-            ));
+           getStructuringElement(
+               cv::MORPH_ELLIPSE,
+               cv::Size(2 * dilationSize + 1, 2 * dilationSize + 1),
+               cv::Point(dilationSize, dilationSize)));
 }
 
-void MyPipeline::pipelineContours(cv::Mat &input, std::vector<std::vector<cv::Point>> &output)
+void MyPipeline::drawAndUpdate(cv::Mat &input, std::vector<std::vector<cv::Point>> &output)
 {
-    cv::Mat copy = input.clone(); // findContours modifies its input
-    std::vector<cv::Vec4i> hierarchy;
+    std::vector<std::vector<cv::Point>> goodContours;
+    cv::Mat copy = input.clone();
+	std::vector<cv::Vec4i> hierarchy;
     findContours(copy, output, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+    for (long unsigned i = 0; i < output.size(); i++)
+    {
+        if (!contourArea(output[i]) < 50)
+        {
+            goodContours.push_back(output[i]);
+        }
+
+    }
+
+    if (goodContours.size() == 1)
+    {
+        std::vector<cv::Rect> boundRect(1);
+        boundRect[0] = boundingRect(goodContours[0]);
+
+        boundingPoints[0] = boundRect[0].tl().x;
+        boundingPoints[1] = boundRect[0].br().y;
+        boundingPoints[2] = boundRect[0].br().x;
+        boundingPoints[3] = boundRect[0].br().y;
+    }
+    else
+    {
+        wpi::outs() << "More than one contour, can't make a bounding box\n";
+    }
+}
+
+double MyPipeline::pxToDegrees(double pixel, int orientation, int imageWidth, int imageHeight) //0 for horizontal 1 for vertical
+{
+    if (orientation == 1) //vertical
+    {
+        return atan(pixel - (imageHeight / 2)) * (verticalFOV / imageHeight);
+    }
+    else if (orientation == 0) //horizontal
+    {
+        return atan(pixel - (imageWidth / 2)) * (horizontalFOV / imageWidth);
+    }
+    else
+    {
+        wpi::outs() << "pxToDegrees called incorrectly\n";
+        return -1;
+    }
+}
+
+void MyPipeline::trajectory(cv::Mat &input)
+{
+    double targetDistance = 0;       // dx in image
+    double targetTheta = 0;          // Red theta in image
+    double boundingBoxMidpointX = 0; // Midpoint of the bounding box on the X axis
+    double imageMidpointX = 0;       // Midpoint of the image on the X axis
+    double yawOffset = 0;            // Yaw offset in degrees
+
+    int imageWidth = input.cols;  // image width
+    int imageHeight = input.rows; // image height
+
+    returnedDistance = getDistance(imageWidth, imageHeight);
+    returnedHorizAngle = getHorizontalAngle(imageWidth, imageHeight);
+}
+
+double MyPipeline::getDistance(int imageWidth, int imageHeight)
+{
+
+    int camTheta = imageHeight - boundingPoints[3];                // Pixel distance between the bottom of the bounding box and bottom of the frame
+    targetTheta = pxToDegrees(camTheta, 1, imageWidth, imageHeight) + angleOffset;
+    targetDistance = targetHeight * atan(targetTheta);
+    return targetDistance;
+}
+
+double MyPipeline::getHorizontalAngle(int imageWidth, int imageHeight)
+{
+    boundingBoxMidpointX = (boundingPoints[0] + boundingPoints[3]) / 2;
+    imageMidpointX = imageWidth / 2; //this might break bc even # of pixels
+    yawOffset = imageMidpointX - boundingBoxMidpointX;
+    return pxToDegrees(yawOffset, 0, imageWidth, imageHeight);
 }
--- jacob_test/MyPipeline.h
+++ jacob_test/MyPipeline.h
@@ -7,15 +7,27 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
 
-class MyPipeline : public frc::VisionPipeline {
-    public:
-    double resultA = 0;
-    double resultB = 0;
-    
+class MyPipeline : public frc::VisionPipeline
+{
+public:
+    double returnedDistance = -1;
+    double returnedHorizAngle = -1;
+
     MyPipeline();
     void Process(cv::Mat &mat) override;
 
-    private:
+private:
+    double targetDistance = 0;       // dx in image
+    double targetTheta = 0;          // Red theta in image
+    double boundingBoxMidpointX = 0; // Midpoint of the bounding box on the X axis
+    double imageMidpointX = 0;       // Midpoint of the image on the X axis
+    double yawOffset = 0;            // Yaw offset in degrees
+	
+    static const double targetHeight;
+    static const double angleOffset;
+    static const double verticalFOV;
+    static const double horizontalFOV;
+
     static const double thresh_hue[2];
     static const double thresh_sat[2];
     static const double thresh_val[2];
@@ -23,16 +35,26 @@ class MyPipeline : public frc::VisionPipeline {
     static const int erosionSize;
     static const int dilationSize;
 
+    double boundingPoints[4];
+
     std::vector<std::vector<cv::Point>> contourResults;
-    
+
     void thresholdHSV(cv::Mat &input, cv::Mat &output,
-                        const double hue[], const double sat[], const double val[]);
-    
+                      const double hue[], const double sat[], const double val[]);
+
     void erodeSize(cv::Mat &input, cv::Mat &output, int size);
 
     void dilateSize(cv::Mat &input, cv::Mat &output, int size);
-    
-    void pipelineContours(cv::Mat &input, std::vector<std::vector<cv::Point>> &output);
+
+    void drawAndUpdate(cv::Mat &input, std::vector<std::vector<cv::Point>> &output);
+
+    void trajectory(cv::Mat &input);
+
+	double pxToDegrees(double pixel, int orientation, int imageWidth, int imageHeight);
+
+    double getDistance(int imageWidth, int imageHeight);
+
+    double getHorizontalAngle(int imageWidth, int imageHeight);
 };
 
 #endif
